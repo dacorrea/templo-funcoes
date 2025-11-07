00796154eb0ef19a90daf05daf3896e5399868ed
Diego Correa <52290901+dacorrea@users.noreply.github.com>
2025-11-07 10:54:28 -0300

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-# Este script é executado automaticamente no deploy do Render
+# render_build.sh
+
+echo "📦 Instalando dependências..."
+pip install -r requirements.txt
 
 echo "📦 Rodando migrações do Django..."
-python manage.py makemigrations gira --noinput
-python manage.py migrate --noinput
+python manage.py makemigrations --noinput || true
+python manage.py migrate --noinput || true
 
 echo "✅ Migrações concluídas."
+
