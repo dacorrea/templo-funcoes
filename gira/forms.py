2001d8ab6a976cf22514236f832a121f4b709dd9
Diego Correa <52290901+dacorrea@users.noreply.github.com>
2025-11-07 16:11:35 -0300

    Update forms.py

--- gira/forms.py
+++ gira/forms.py
@@ -4,6 +4,13 @@ from .models import Funcao, Gira, Medium
 class LoginPhoneForm(forms.Form):
     celular = forms.CharField(max_length=20)
 
+class LoginForm(forms.Form):
+    celular = forms.CharField(
+        label='Celular',
+        max_length=20,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Digite seu celular'})
+    )
+
 class GiraForm(forms.ModelForm):
     class Meta:
         model = Gira
