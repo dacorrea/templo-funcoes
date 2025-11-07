831b36d3c0ae67a12797f7303383af7c5805827e
Diego Correa <52290901+dacorrea@users.noreply.github.com>
2025-11-07 08:52:59 -0300

    Update wsgi.py

--- templo_project/wsgi.py
+++ templo_project/wsgi.py
@@ -1,5 +1,17 @@
 import os
 from django.core.wsgi import get_wsgi_application
+import django
+from django.core.management import call_command
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'templo_project.settings')
+django.setup()
+try:
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Erro ao aplicar migrações: {e}")
+
+application = get_wsgi_application()
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'templo_project.settings')
 application = get_wsgi_application()
